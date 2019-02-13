a84e638848730ca1c7332f15f1e0b67c8196964a
Shani Raby <shani.raby@gmail.com>
2019-02-13 10:55:33 +0200

    feat: change a to do multi

--- js/hello_world.js
+++ js/hello_world.js
@@ -1,10 +1,10 @@
 function main() {
   console.log('Hello world!');
-  console.log('a() ===', a(8,9));
+  console.log('a() ===', a(3,4));
 }
 
 function a(x, y) {
-  return x + y;
+  return x * y;
 }
 
 if (require.main === module) {
